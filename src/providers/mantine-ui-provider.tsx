25731ee0ee5bdc3ec3b34d34d0576f67998b1c98
LiusDev <quydx.work@gmail.com>
2025-03-17 08:55:26 +0700

    fix: theme

--- src/providers/mantine-ui-provider.tsx
+++ src/providers/mantine-ui-provider.tsx
@@ -5,31 +5,31 @@ import "@mantine/charts/styles.css"
 import "@mantine/dropzone/styles.css"
 import "mantine-datatable/styles.layer.css"
 
-import { createTheme, MantineColorsTuple, MantineProvider } from "@mantine/core"
+import { MantineProvider } from "@mantine/core"
 import { Notifications } from "@mantine/notifications"
 import { ModalsProvider } from "@mantine/modals"
 import { DrawerProvider } from "./drawer-provider"
 import { DatesProvider } from "@mantine/dates"
 
-const myColor: MantineColorsTuple = [
-	"#ffe9ff",
-	"#fed1fd",
-	"#faa1f6",
-	"#f66ef1",
-	"#f243eb",
-	"#f028e9",
-	"#f018e8",
-	"#d609ce",
-	"#bf00b9",
-	"#a700a1",
-]
+// const myColor: MantineColorsTuple = [
+// 	"#ffe9ff",
+// 	"#fed1fd",
+// 	"#faa1f6",
+// 	"#f66ef1",
+// 	"#f243eb",
+// 	"#f028e9",
+// 	"#f018e8",
+// 	"#d609ce",
+// 	"#bf00b9",
+// 	"#a700a1",
+// ]
 
-const theme = createTheme({
-	colors: {
-		myColor,
-	},
-	primaryColor: "myColor",
-})
+// const theme = createTheme({
+// 	colors: {
+// 		myColor,
+// 	},
+// 	primaryColor: "myColor",
+// })
 
 export const MantineUIProvider = ({
 	children,
@@ -37,7 +37,7 @@ export const MantineUIProvider = ({
 	children: React.ReactNode
 }) => {
 	return (
-		<MantineProvider theme={theme}>
+		<MantineProvider>
 			<DatesProvider settings={{ locale: "vi" }}>
 				<ModalsProvider>
 					<DrawerProvider>
